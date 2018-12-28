fc80125127fe7ccf019bafe12cbe61b5e0f4cf14
antjanus <antonin@antjanus.com>
2018-12-27 23:35:56 -0700

    feat(ui): Initial styles

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const JSX = (<h1>Skok Photo Manager</h1>);
+const JSX = (
+  <div>
+    <header>
+      <h1 className="title">
+        <span className="text-primary">Skok</span> Photo Manager
+      </h1>
+    </header>
+    <main>
+      <div>
+        <input type="text" placeholder="Directory path"/>
+      </div>
+      <br />
+      <a href="#" className="button-primary">Scan photo directory</a>
+    </main>
+  </div>
+);
 
 ReactDOM.render(JSX, document.getElementById('root'));
