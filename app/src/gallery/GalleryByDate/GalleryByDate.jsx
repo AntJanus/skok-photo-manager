28c08fec93b0b2213493cc01fd3069a7b6f0926f
Antonin Januska <antonin@antjanus.com>
2019-07-22 21:57:00 -0600

    feat(app): Gallery by date component

---
+++ app/src/gallery/GalleryByDate/GalleryByDate.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { transponder } from '../../services/transponder';
+
+import { Link } from 'react-router-dom';
+
+export function GalleryByDate() {
+  return (
+    <div>By Date</div>
+  );
+}
